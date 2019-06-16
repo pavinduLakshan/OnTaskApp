eb472dc53c54df916389baf1bb865e3aa970c2ae
pavinduLakshan <174084F@uom.lk>
2019-06-16 17:04:01 +0530

    started designing group screen layout

--- screens/GroupsScreen.js
+++ screens/GroupsScreen.js
@@ -1,22 +1,52 @@
 import React from "react";
-import { View, Text,Button } from "react-native";
+import { View, Text, Button, FlatList } from "react-native";
+
+const styles = {
+  item: { 
+      flex: 1, 
+      flexDirection: "row", 
+      padding: "2%",
+      borderTopWidth: 1,
+      borderColor: "green" },
+};
 
 export default class Groups extends React.Component {
-    static navigationOptions = {
-      drawerLabel: 'Groups'
-    }
+  static navigationOptions = {
+    drawerLabel: "Groups",
+  };
 
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>Groups</Text>
-          <Button
-          title="Back to signup"
-          onPress={() =>
-            this.props.navigation.openDrawer()
-          }
+  render() {
+    return (
+      <View>
+        <Button
+          title="Menu"
+          onPress={() => this.props.navigation.openDrawer()}
         />
-        </View>
-      );
-    }
+        <Text style={{ fontSize: 40,padding: "2%" }}>Groups</Text>
+        <FlatList
+          data={[
+            { key: "Devin" },
+            { key: "Jackson" },
+            { key: "James" },
+            { key: "Joel" },
+            { key: "John" },
+            { key: "Jillian" },
+            { key: "Jimmy" },
+            { key: "Julie" },
+          ]}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <View>
+                <Text>Image</Text>
+              </View>
+              <View>
+                <Text style={{ fontSize: 20 }}>{item.key}</Text>
+                <Text>Last activity</Text>
+              </View>
+            </View>
+          )}
+        />
+      </View>
+    );
   }
+}
