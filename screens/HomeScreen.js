8fe9c9cc71521283f27fb6118de84e9de3baf2fb
pavinduLakshan <174084F@uom.lk>
2019-06-21 00:59:08 +0530

    added random image to display based on time

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text,Button } from "react-native";
+import { View, Text,Button,Image } from "react-native";
 import { DrawerActions } from 'react-navigation';
 
 export default class Home extends React.Component {
@@ -7,17 +7,38 @@ export default class Home extends React.Component {
       drawerLabel: 'Home'
     }
 
+    state={
+      mood: ""
+    }
+
+    componentDidMount(){
+      var today = new Date()
+      var curHr = today.getHours()
+      
+      if (curHr < 12) {
+        this.setState({mood: "morning"})
+      } else if (curHr < 18) {
+        this.setState({mood: "evening"})
+      } else {
+        this.setState({mood: "night"})
+    }
+  }
+
     render() {
       return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>Home Screen</Text>
+        <View>
           <Button
-          title="Back to Home"
+          title="Menu"
           onPress={() =>
-            //this.props.navigation.navigate('drawerOpen')
             this.props.navigation.dispatch(DrawerActions.openDrawer())
           }
         />
+         <Image
+         resizeMode = 'cover'
+                   style={{ height: 250}}
+          source={{ uri: `https://source.unsplash.com/featured/?${this.state.mood},nature`}}
+        />
+
         </View>
       );
     }
