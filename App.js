ff8493f34f40cb7a0eba8075245772cdbeb0e9e6
pavinduLakshan <174084F@uom.lk>
2019-07-16 21:51:06 +0530

    installed and configured axios

--- App.js
+++ App.js
@@ -10,6 +10,28 @@ import DrawerNavigator from './Utils/DrawerNavigator'
 import {
   createAppContainer
 } from 'react-navigation';
+import axios from 'axios'
+import AsyncStorage from "@react-native-community/async-storage"
+
+// const request = axios.create({
+//   baseURL: 'http://192.168.1.100:8080/api',
+// })
+
+axios.defaults.baseURL='http://192.168.1.100:8080/api'
+
+axios.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = "Bearer "+token
+    }
+    console.log("Hey")
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+);
 
 const App = createAppContainer(DrawerNavigator);
 
--- Utils/SENDER.js
+++
@@ -1,9 +0,0 @@
-import axios from 'react-native-axios'
-
-export default axios.create({
-        baseURL: 'http://192.168.1.100:8080/api',
-        headers: {
-          //'Authorization': 'Bearer '+ localStorage.getItem('token')
-          'Content-Type': 'application/json'
-        }
-      })
